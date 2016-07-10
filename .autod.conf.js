50f1729045da3ed520b5455f3406999f1ac1e1da
dead-horse <dead_horse@qq.com>
2016-07-10 12:46:58 +0800

    init code

---
+++ .autod.conf.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = {
+  write: true,
+  prefix: '^',
+  devprefix: '^',
+  exclude: [
+    'test/fixtures',
+  ],
+  devdep: [
+    'autod',
+    'egg-bin',
+    'eslint',
+    'eslint-config-egg'
+  ]
+};
