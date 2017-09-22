30d120dd292d79a25113c951664d18480dde2a00
Haoliang Gao <sakura9515@gmail.com>
2017-09-22 11:02:09 +0800

    fix: should send egg-ready when app/agent restarted after launched (#45)

--- test/fixtures/apps/egg-ready/agent.js
+++ test/fixtures/apps/egg-ready/agent.js
@@ -1,6 +1,9 @@
 'use strict';
 
 module.exports = function(agent) {
+  agent.messenger.on('throw', () => {
+    process.exit(1);
+  });
   agent.messenger.on('egg-ready', data => {
     console.log(`agent receive egg-ready, with ${data.workers} workers`);
   });
