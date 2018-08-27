b0c8d19e08495c596045eada460590562a4150be
killa <killa123@126.com>
2018-08-27 17:49:20 +0800

    fix: fix messenger#sendTo (#74)
    `sendTo` should can send to master, agent, worker as doc mentioned.
    
    > app.messenger.sendTo(pid, action, data): send messages to specified process

--- test/fixtures/apps/messenger/agent.js
+++ test/fixtures/apps/messenger/agent.js
@@ -1,5 +1,11 @@
 'use strict';
 
+const pids = {
+  master: process.ppid,
+  worker: new Set(),
+  agent: process.pid,
+};
+
 module.exports = function(agent) {
   // from parent
   agent.messenger.on('parent2agent', msg => console.log(msg));
@@ -21,4 +27,39 @@ module.exports = function(agent) {
 
   // compatible with string
   agent.messenger.on('app2agentbystring', msg => console.log('agent: ' + msg));
+
+  agent.messenger.on('egg-ready', () => {
+    agent.messenger.sendToApp('worker_online', { type: 'agent', pid: process.pid });
+  });
+
+  agent.messenger.on('worker_online', data => {
+    workerOnline(data);
+    sendToProcess(agent.messenger);
+  });
+
+  agent.messenger.on('send_to_pid', data => {
+    if (data.type === 'app') {
+      console.log('app sendTo agent done');
+    }
+    if (data.type === 'agent') {
+      console.log('agent sendTo agent done');
+    }
+  });
 };
+
+function workerOnline(data) {
+  if (data.type === 'agent') {
+    pids.agent = data.pid;
+  } else {
+    pids.worker.add(data.pid);
+  }
+}
+
+function sendToProcess(messenger) {
+  const data = { type: 'agent', fromProcess: process.pid };
+  messenger.sendTo(pids.master, 'send_to_pid', data);
+  messenger.sendTo(pids.agent, 'send_to_pid', data);
+  for (const pid of pids.worker) {
+    messenger.sendTo(pid, 'send_to_pid', data);
+  }
+}
