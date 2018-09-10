5d906cb9d74f3cb2d82ca2c5f23e888c6bfbece6
waiting <1661926154@qq.com>
2018-09-10 19:41:08 +0800

    feat(typings): add index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,22 @@
+import { SecureContextOptions } from 'tls';
+
+
+export interface ClusterOptions {
+  /** specify framework that can be absolute path or npm package */
+  framework?: string;
+  /** directory of application, default to `process.cwd()` */
+  baseDir?: string;
+  /** customized plugins, for unittest */
+  plugins?: object | null;
+  /** numbers of app workers, default to `os.cpus().length` */
+  workers?: number;
+  /** listening port, default to 7001(http) or 8443(https) */
+  port?: number;
+  /** Ref: https://nodejs.org/api/tls.html#tls_tls_createsecurecontext_options */
+  https?: SecureContextOptions;
+  [prop: string]: any;
+}
+
+
+
+export function startCluster(options: ClusterOptions, callback: (...args: any[]) => any): void;
