43f2cb313202ce7897d2d80fe92f984a8998ea74
waiting <1661926154@qq.com>
2018-09-11 15:18:01 +0800

    refactor: move reading cert file into parseTLSOpts()
    validate cert file exists during start worker

--- test/tls_options.test.js
+++ test/tls_options.test.js
@@ -10,8 +10,7 @@ const parseTLSOpts = tlsUtil.parseTLSOpts;
 describe('test/tls_options.test.js', () => {
   describe('Should parseTLSOpts()', () => {
     it('with https:true', () => {
-      const ret = parseTLSOpts(true);
-      assert(typeof ret === 'undefined');
+      assert.throws(() => parseTLSOpts(true));
     });
 
     it('with https:false', () => {
@@ -29,52 +28,118 @@ describe('test/tls_options.test.js', () => {
       assert(typeof ret === 'undefined');
     });
 
-    it('with https:key/cert by file type', () => {
+    it('with invalid https.ca file path', () => {
+      const opts = {
+        ca: Math.random().toString(),
+      };
+      assert.throws(
+        () => parseTLSOpts(opts),
+        new RegExp(`File of https.ca should exists: "${opts.ca}"`)
+      );
+
+      opts.ca = '';
+      assert.throws(
+        () => parseTLSOpts(opts),
+        new RegExp(`File of https.ca should exists: "${opts.ca}"`)
+      );
+    });
+
+    it('with invalid https.cert file path', () => {
+      const opts = {
+        cert: Math.random().toString(),
+      };
+      assert.throws(
+        () => parseTLSOpts(opts),
+        new RegExp(`File of https.cert should exists: "${opts.cert}"`)
+      );
+
+      opts.cert = '';
+      assert.throws(
+        () => parseTLSOpts(opts),
+        new RegExp(`File of https.cert should exists: "${opts.cert}"`)
+      );
+    });
+
+    it('with invalid https.key file path', () => {
+      const opts = {
+        key: Math.random().toString(),
+      };
+      assert.throws(
+        () => parseTLSOpts(opts),
+        new RegExp(`File of https.key should exists: "${opts.key}"`)
+      );
+
+      opts.key = '';
+      assert.throws(
+        () => parseTLSOpts(opts),
+        new RegExp(`File of https.key should exists: "${opts.key}"`)
+      );
+    });
+
+    it('with invalid https.pfx file path', () => {
+      const opts = {
+        pfx: Math.random().toString(),
+      };
+      assert.throws(
+        () => parseTLSOpts(opts),
+        new RegExp(`File of https.pfx should exists: "${opts.pfx}"`)
+      );
+
+      opts.pfx = '';
+      assert.throws(
+        () => parseTLSOpts(opts),
+        new RegExp(`File of https.pfx should exists: "${opts.pfx}"`)
+      );
+    });
+
+    it('with https:key/cert by file path', () => {
       const key = join(__dirname, 'fixtures/server.key');
       const cert = join(__dirname, 'fixtures/server.crt');
+      const pfx = join(__dirname, 'fixtures/server.crt');
       const opts = {
         key,
         cert,
+        pfx,
       };
       const ret = parseTLSOpts(opts);
 
-      assert(ret && Object.keys(ret).length === 2);
-      assert(ret.key === key);
-      assert(ret.cert === cert);
+      assert(ret && Object.keys(ret).length === 3);
+      assert(ret.key.toString() === readFileSync(key).toString());
+      assert(ret.cert.toString() === readFileSync(cert).toString());
+      assert(ret.pfx.toString() === readFileSync(cert).toString());
     });
 
-    it('with https:key/cert by Buffer type', () => {
+    it('with https:key/cert by Buffer', () => {
       const key = join(__dirname, 'fixtures/server.key');
       const cert = join(__dirname, 'fixtures/server.crt');
       const opts = {
         key: readFileSync(key),
         cert: readFileSync(cert),
+        pfx: readFileSync(cert),
       };
       const ret = parseTLSOpts(opts);
 
-      assert(ret && Object.keys(ret).length === 2);
+      assert(ret && Object.keys(ret).length === 3);
       assert(ret.key.toString() === opts.key.toString());
       assert(ret.cert.toString() === opts.cert.toString());
+      assert(ret.pfx.toString() === opts.pfx.toString());
     });
   });
 
 
   describe('Should mergeTLSOpts()', () => {
     it('with both optionsHttps and listenHttps invalid', () => {
-      let ret = mergeTLSOpts(true, true);
-      assert(typeof ret === 'undefined');
-
-      ret = mergeTLSOpts({}, {});
+      let ret = mergeTLSOpts({}, {});
       assert(typeof ret === 'undefined');
 
       ret = mergeTLSOpts({});
       assert(typeof ret === 'undefined');
 
-      ret = mergeTLSOpts(true);
-      assert(typeof ret === 'undefined');
-
       ret = mergeTLSOpts();
       assert(typeof ret === 'undefined');
+
+      assert.throws(() => mergeTLSOpts(true));
+      assert.throws(() => mergeTLSOpts(true, true));
     });
 
     it('with valid optionsHttps and invalid listenHttps', () => {
@@ -97,9 +162,7 @@ describe('test/tls_options.test.js', () => {
       assert(ret && Object.keys(ret).length === 2);
       assert(ret && ret.key === opts.key && ret.cert === opts.cert);
 
-      ret = mergeTLSOpts(true, opts);
-      assert(ret && Object.keys(ret).length === 2);
-      assert(ret && ret.key === opts.key && ret.cert === opts.cert);
+      assert.throws(() => mergeTLSOpts(true, opts));
 
       ret = mergeTLSOpts(false, opts);
       assert(ret && Object.keys(ret).length === 2);
