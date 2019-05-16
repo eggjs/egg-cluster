0fd73072d94f0fdb30ae766a6ae5a9f36bea71c5
Suyi Thonatos.Yang <thonatos.yang@gmail.com>
2019-05-16 14:41:56 +0800

    test: add test for https

---
+++ test/https.test.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const assert = require('assert');
+const mm = require('egg-mock');
+const urllib = require('urllib');
+const path = require('path');
+const parseOptions = require('../lib/utils/options');
+const utils = require('./utils');
+
+describe('test/https.test.js', () => {
+  let app;
+
+  afterEach(mm.restore);
+
+  describe('https server', () => {
+    afterEach(() => app.close());
+
+    it('should success with status 200', function* () {
+
+      const baseDir = path.join(__dirname, 'fixtures/apps/https-server');
+      const options = parseOptions({
+        baseDir,
+        https: {
+          key: utils.getFilepath('server.key'),
+          cert: utils.getFilepath('server.cert'),
+        },
+      });
+      app = utils.cluster('apps/https-server', options);
+      yield app.ready();
+
+      const response = yield urllib.request('https://127.0.0.1:8443', {
+        rejectUnauthorized: false,
+      });
+
+      assert(response.status === 200);
+      assert(response.data.toString(), 'https server');
+    });
+
+  });
+});
